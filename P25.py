7cc535dcced2f94281a41423bba0f82e5e86aeab
Magnum-Mange <magnus.sonnerlind@gmail.com>
2018-02-26 14:26:46 +0100

    Completed the formation acquisition part.

--- P25.py
+++ P25.py
@@ -21,10 +21,25 @@ class Node():
     def dist(self, otherNode):
         return np.linalg.norm(otherNode.pos - self.pos)
 
-def formationAcquisition(mapp):
-    mapp.formation_positions = mapp.formation_positions[1:]
+class Formation():
+
+    def __init__(self, formationPositions):
+        self.leaderPosition = formationPositions[0]
+        self.followerPositions = formationPositions[1:]
+        self.distances = np.linalg.norm(self.followerPositions - self.leaderPosition, axis = 1)
+        self.angles = np.angle(self.followerPositions[:, 0] - self.leaderPosition[0] + 1j * (self.followerPositions[:, 1] - self.leaderPosition[1])) - np.pi / 2
+
+    def getFollowerPositions(self, currentLeaderPosition, currentLeaderAngle):
+        return currentLeaderPosition + np.array([np.multiply(self.distances, np.cos(self.angles + currentLeaderAngle)), np.multiply(self.distances, np.sin(self.angles + currentLeaderAngle))]).T
+
+def formationAcquisition(mapp, formation):
+##    mapp.formation_positions = mapp.formation_positions[1:]
+    leaderStartPosition = np.array([mapp.traj_x[0], mapp.traj_y[0]])
+    leaderStartAngle = mapp.traj_theta[0]
+    formationPositions = formation.getFollowerPositions(leaderStartPosition, leaderStartAngle)
     paths = []
-    thetas = np.angle(mapp.formation_positions[:, 0] - mapp.start_positions[:, 0] + 1j * (mapp.formation_positions[:, 1] - mapp.start_positions[:, 1]))
+##    thetas = np.angle(mapp.formation_positions[:, 0] - mapp.start_positions[:, 0] + 1j * (mapp.formation_positions[:, 1] - mapp.start_positions[:, 1]))
+    thetas = np.angle(formationPositions[:, 0] - mapp.start_positions[:, 0] + 1j * (formationPositions[:, 1] - mapp.start_positions[:, 1]))
     TOLERANCE = mapp.v_max * mapp.dt * 8
     DECELERATION_TOLERANCE = mapp.v_max ** 2 / (2 * mapp.a_max)
 
@@ -32,7 +47,7 @@ def formationAcquisition(mapp):
     for pos in mapp.start_positions:
         tree = []
         startNode = Node(pos, thetas[i], 0)
-        formationNode = Node(mapp.formation_positions[i], 0, 0)
+        formationNode = Node(formationPositions[i], 0, 0)
         tree.append(startNode)
         currentNode = startNode
 
@@ -84,5 +99,7 @@ def formationAcquisition(mapp):
     paths.append(path)    
 
 ##    for tree in trees:
-    
-trees = formationAcquisition(Map.Map("P25.json", "P25_26_traj.json"))
+
+formation = Formation(Map.Map("P25.json", "P25_26_traj.json").formation_positions)    
+trees = formationAcquisition(Map.Map("P25.json", "P25_26_traj.json"), formation)
+print(formation.getFollowerPositions(np.array([10.1, 15]), np.pi / 2))
